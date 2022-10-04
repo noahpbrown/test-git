f541ecfb819695e81b6a0c85353a1fb04c5319e3
Noah Brown <npb2703@gmail.com>
2022-10-04 16:58:42 -0400

    Initial Commit

---
+++ test-git/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test-git
+//
+//  Created by Noah Brown on 10/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test-git/test_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_gitApp.swift
+//  test-git
+//
+//  Created by Noah Brown on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct test_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
